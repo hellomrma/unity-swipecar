9decc300b0df53dee0bcb34d57d937fe88a9b89b
Noah Ma (GwangSeub) <hellomrma@hivelab.co.kr>
2025-12-28 13:41:55 +0900

    Update

--- Assets/02.Scripts/CarController2.cs
+++ Assets/02.Scripts/CarController2.cs
@@ -5,8 +5,11 @@ public class CarController2 : MonoBehaviour
     public float carSpeed = 0f;
 
     Vector2 startPosition;
-    bool isDragging = false;  // 드래그 진행 중인지 확인하는 플래그
+    public bool isDragging = false;  // 드래그 진행 중인지 확인하는 플래그
+    public bool isCanStart = false;
 
+    // 새로 추가: Restart 버튼 상태를 확인하기 위한 참조(인스펙터에 할당)
+    public GameObject restartButton;
 
     void Update()
     {
@@ -16,8 +19,9 @@ public class CarController2 : MonoBehaviour
         // GetMouseButtonUp(0): 마우스 버튼을 떼는 순간 한 번만 true 반환 (단발 입력 감지)
 
 
-        // 드래그가 진행 중이 아니면 언제든지 새로운 드래그 시작 가능
-        if (Input.GetMouseButtonDown(0) && !isDragging)
+        // 드래그 시작 허용 조건: 드래그 중이 아니고 차량이 거의 정지 상태이며 restart 버튼이 활성화되어 있지 않을 때만 허용
+        bool restartActive = restartButton != null && restartButton.activeSelf;
+        if (Input.GetMouseButtonDown(0) && !isDragging && Mathf.Abs(carSpeed) <= 0.005f && !restartActive)
         {
             startPosition = Input.mousePosition;
             isDragging = true;  // 드래그 시작
@@ -31,9 +35,9 @@ public class CarController2 : MonoBehaviour
 
             carSpeed = (dragGap * 0.005f) * 0.23f;
             isDragging = false;  // 드래그 종료
+            isCanStart = true;
             GetComponent<AudioSource>().Play();
         }
-
 
         transform.Translate(carSpeed, 0, 0);
 
--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,10 +11,12 @@ public class GameManager : MonoBehaviour
     public GameObject flag;
     public Text distance;
 
+    public GameObject restartButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        restartButton.SetActive(false);
     }
 
     // Update is called once per frame
@@ -26,5 +29,21 @@ public class GameManager : MonoBehaviour
         {
             distance.text = "You reached the flag!";
         }
+
+        if (car.GetComponent<CarController2>().isCanStart == true && car.GetComponent<CarController2>().carSpeed == 0)
+        {
+            restartButton.SetActive(true);
+        }
+    }
+
+    public void RestartGame()
+    {
+        car.transform.position = new Vector3(-6.74f, car.transform.position.y, car.transform.position.z);
+        car.GetComponent<CarController2>().carSpeed = 0f;
+        car.GetComponent<CarController2>().isCanStart = false;
+        car.GetComponent<CarController2>().isDragging = false;
+        restartButton.SetActive(false);
+
+        // SceneManager.LoadScene(0); // Alternatively, reload the entire scene
     }
 }
